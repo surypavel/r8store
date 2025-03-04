bed629ca7f4ad02eabd34e65746577eba4990021
Pavel Surý <pavel.sury@rossum.ai>
2025-03-04 13:33:07 +0100

    Attach v2

--- dist/add_attachment/code.js
+++ dist/add_attachment/code.js
@@ -1,54 +1,89 @@
+const node_fetch = require("node-fetch");
+
 const getConfigureIntent = () => {
-    return {
-      intent: {
-        form: {
-          width: 500,
-          hook,
-          schema: {
-            type: 'object',
-            properties: {
-              annotation: {
-                type: 'string'
-              },
+  return {
+    intent: {
+      form: {
+        width: 500,
+        schema: {
+          type: "object",
+          properties: {
+            annotation: {
+              type: "string",
             },
-            required: ['annotation'],
           },
-          uiSchema: {
-            "type": "VerticalLayout",
-            "label": "Target annotation ID",
-            "elements": [{
-              type: 'Control',
-              scope: '#/properties/annotation',
-            }, ]
-          }
-        }
-      }
-    };
-  }
-  
-  exports.rossum_hook_request_handler = async ({
-    rossum_authorization_token,
-    base_url,
-    action
-  }) => {
-    if (action === "configure") {
-      return getConfigureIntent();
-    } else {
-      return await fetch(`${base_url}/api/v1/relations`, {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          "Authorization": `Token ${rossum_authorization_token}`
+          required: ["annotation"],
         },
-        body: JSON.stringify({
-          "type": "attachment",
-          "parent": "https://<example>.rossum.app/api/v1/annotations/123",
-          "annotations": ["https://<example>.rossum.app/api/v1/annotations/124"],
-        })
-      }).then(async (mutation) => {
-        const message = JSON.stringify(await mutation.json());
-        return mutation.status === 200 ? { intent: { success: { message: "Asdf" } } } : { intent: { error: { message: message } }}
-      })
-    }
+        uiSchema: {
+          type: "VerticalLayout",
+          elements: [
+            {
+              label: "Target annotation ID",
+              type: "Control",
+              scope: "#/properties/annotation",
+            },
+          ],
+        },
+      },
+    },
   };
+};
+
+exports.rossum_hook_request_handler = async ({
+  rossum_authorization_token,
+  base_url,
+  ui_action,
+  payload,
+  annotation,
+}) => {
+  if (ui_action === "configure") {
+    return getConfigureIntent();
+  } else {
+    const targetAnnotation = await node_fetch(
+      `${base_url}/api/v1/annotations/${payload.annotation}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Token ${rossum_authorization_token}`,
+        },
+      }
+    ).then((targetAnnotation) => targetAnnotation.json());
+
+    const sourceAnnotation = await node_fetch(
+      `${base_url}/api/v1/annotations/${annotation.id}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Token ${rossum_authorization_token}`,
+        },
+      }
+    ).then((sourceAnnotation) => sourceAnnotation.json());
+
+    return await node_fetch(`${base_url}/api/v1/relations`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Token ${rossum_authorization_token}`,
+      },
+      body: JSON.stringify({
+        type: "attachment",
+        parent: sourceAnnotation.url,
+        annotations: [targetAnnotation.url],
+      }),
+    }).then(async (mutation) => {
+      const message = JSON.stringify(await mutation.json());
+      return mutation.status === 200
+        ? {
+            intent: {
+              success: { message: "Annotation was attached successfully." },
+            },
+          }
+        : { intent: { error: { message: message } } };
+    });
+  }
+};
