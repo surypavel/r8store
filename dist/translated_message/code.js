3e4290aae20fefa0cfef5e04080a2edba97abc22
Pavel Surý <pavel.sury@rossum.ai>
2025-05-20 14:06:25 +0200

    Update translated messages

--- dist/translated_message/code.js
+++ dist/translated_message/code.js
@@ -1,4 +1,4 @@
-const getIDFromUrl = (url) => Number(url.split('/').pop())
+const getIDFromUrl = (url) => Number(url.split("/").pop());
 
 exports.rossum_hook_request_handler = async ({
   rossum_authorization_token,
@@ -10,39 +10,50 @@ exports.rossum_hook_request_handler = async ({
 }) => {
   if (configure === true) {
     return {
-      "intent": {
-        "form": {
-          "schema": {
-            type: 'object',
-            properties: Object.fromEntries(settings.locales.map(locale => ([locale, {
-              type: 'string',
-            }])))
+      intent: {
+        form: {
+          schema: {
+            type: "object",
+            properties: Object.fromEntries(
+              settings.locales.map((locale) => [
+                locale,
+                {
+                  type: "string",
+                },
+              ])
+            ),
           },
-        }
-      }
-    }
+        },
+      },
+    };
   } else {
-    const endpoint = `${base_url}/api/v1/users/${getIDFromUrl(annotation.modified_by)}`;
+    const endpoint = `${base_url}/api/v1/users/${getIDFromUrl(
+      annotation.modified_by
+    )}`;
     const response = await fetch(endpoint, {
       method: "GET",
       headers: {
-        "Authorization": `Bearer ${rossum_authorization_token}`
-      }
+        Authorization: `Bearer ${rossum_authorization_token}`,
+      },
     });
 
     const user = await response.json();
 
-    return payloads.flatMap(payload => {
-      const content = payload[user.ui_settings.locale];
+    return {
+      messages: payloads.flatMap((payload) => {
+        const content = payload[user.ui_settings.locale];
 
-      // Do stuff with payload and rossum token and annotation.
-      return content ? {
-        "messages": [{
-          type: "warning",
-          content,
-          id: "all"
-        }]
-      } : []  
-    })
+        // Do stuff with payload and rossum token and annotation.
+        return content
+          ? [
+                {
+                  type: "warning",
+                  content,
+                  id: "all",
+                },
+              ]
+          : [];
+      }),
+    };
   }
 };
