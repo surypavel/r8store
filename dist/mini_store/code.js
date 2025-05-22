ddff56d84cff51db3071c1e6cf55d6251a8445a2
Pavel Surý <pavel.sury@rossum.ai>
2025-05-22 14:15:41 +0200

    Simplify mini store

--- dist/mini_store/code.js
+++ dist/mini_store/code.js
@@ -1,38 +1,15 @@
 const custom = {
-  url: "https://elis.develop.r8.lol/api/v1/hook_templates/25",
-  metadata: {},
-  events: [],
-  test: {},
-  settings: {},
-  settings_schema: null,
-  secrets_schema: {
-    type: "object",
-    additionalProperties: {
-      type: "string",
+    "events": [],
+    "config": {
+      "code": `exports.rossum_hook_request_handler = async ({ payload }) => {};`,
+      "runtime": "nodejs22.x"
     },
-  },
-  config: {
-    url: "",
-  },
-  settings_description: [],
-  token_lifetime_s: null,
-  name: "Coupa Integration",
-  type: "webhook",
-  sideload: [],
-  description:
-    "Integrate Rossum with Coupa. Automatically export Documents to Coupa, replicate Master Data in Rossum, and keep them up to date.",
-  extension_source: "rossum_store",
-  guide: "",
-  read_more_url: null,
-  extension_image_url:
-    "https://rossum.s3.eu-west-1.amazonaws.com/rossum-store-assets/sap/Coupa+Integration.png",
-  store_description:
-    '<div style="line-height: 1.8;">\r\n\r\n    <img style="padding-bottom: 20px; width: 100%"\r\n         src="https://rossum.s3.eu-west-1.amazonaws.com/rossum-store-assets/sap/Coupa+Integration.png"\r\n         alt="Coupa Integration picture">\r\n\r\n<p>Simplify your operations with the integration of Rossum and Coupa. Streamline your document export to Coupa, maintain synchronized master data in Rossum, and ensure up-to-date records.</p>\r\n\r\n<p>The Coupa Integration is a pre-configured integration by Rossum, facilitating seamless transfer of invoice data from Rossum to Coupa. This integration accelerates processing, relieving your Accounts Payable team from low value manual tasks, and assures invoice accuracy with minimal human intervention.</p>\r\n\r\n<p>The solution features Coupa\'s master data replication in Rossum. The custom business logic configuration ensures precise extraction, matching, or calculation of data for reliable invoice creation in Coupa and export of extracted invoice data to Coupa.</p>\r\n\r\n<p>The Coupa Integration leverages SFTP for two-way data communication with Coupa, leading to successful document generation in Coupa. See all the features of this product in our <a href="https://rossum.ai/help/wp-content/uploads/2022/10/Coupa-Integration-Service-Product-Sheet-.pdf"><b>product sheet</b></a>.</p>\r\n\r\n<p>This integration is a paid add-on, request a demo below and get a quote.</p>',
-  external_url: "https://go.rossum.ai/coupa-extention-request",
-  use_token_owner: true,
-  order: 6,
-  install_action: "request_access",
-};
+    "description": "Custom added extension.",
+    "name": "Custom added extension",
+    "type": "function",
+    "store_description": "Check for sender_address of a current annotation and check the weather (temperature). This shows how you can communicate with external APIs.",
+    "hook_integrations": []
+  };
 
 exports.rossum_hook_request_handler = async ({ payload }) => {
   if (payload["name"] == "get_hook_template_list") {
