d3059766ff9fd768cf2372e8dafa7835795c2960
Pavel Surý <pavel.sury@rossum.ai>
2025-06-23 20:23:32 +0200

    Add AI lookup

---
+++ dist/lookup_ai/code.js
@@ -0,0 +1,73 @@
+const fstring = {
+  oneOf: [
+    {
+      type: "string",
+    },
+    {
+      type: "object",
+      properties: {
+        __fstring: {
+          type: "string",
+        },
+      },
+    },
+  ],
+};
+
+exports.rossum_hook_request_handler = async ({ configure, payload, base_url }) => {
+  if (configure === true) {
+    return {
+      intent: {
+        uiSchema: {
+          type: "VerticalLayout",
+          elements: [
+            {
+              type: "FString",
+              scope: "#/properties/prompt",
+            },
+          ],
+        },
+        form: {
+          schema: {
+            definitions: {
+              fstring,
+            },
+            type: "object",
+            properties: {
+              prompt: {
+                $ref: "#/definitions/fstring",
+              },
+            },
+          },
+        },
+      },
+    };
+  } else {
+    const endpoint = `${base_url}/api/v1/internal/chatbot`;
+    const response = await fetch(endpoint, {
+      method: "POST",
+      body: JSON.stringify({
+        model: "anthropic.claude-v2",
+        messages: [
+          {
+            role: "user",
+            content: `${payload.prompt}. Separate each entry with comma. Do not add any additional text.`,
+          },
+        ],
+      }),
+      headers: {
+        Authorization: `Bearer ${rossum_authorization_token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await response.json();
+    const options = body.messages
+      .find((message) => message.role === "system")
+      .content.split(",");
+
+    return {
+      options,
+      value: options[0],
+    };
+  }
+};
