f96e839195117f85c8cb23f85757b268aa28f0a7
Pavel Surý <pavel.sury@rossum.ai>
2025-06-23 13:55:36 +0200

    Add lookup mdh

---
+++ dist/lookup_mdh/code.js
@@ -0,0 +1,64 @@
+exports.rossum_hook_request_handler = async ({ configure, secrets, payload, settings }) => {
+  const url = settings.url || "https://elis.master.r8.lol/svc/master-data-hub/api"
+
+  if (configure === true) {
+    const datasets = await fetch(
+      `${url}/v2/dataset/`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${secrets.token}`,
+        },
+      }
+    );
+
+    return {
+      intent: {
+        form: {
+          schema: {
+            type: "object",
+            properties: {
+              dataset: {
+                type: "string",
+                enum: await datasets.json(),
+              },
+              value_key: {
+                type: "string",
+              },
+              label_key: {
+                type: "string",
+              },
+            },
+          },
+        },
+      },
+    };
+  } else {
+    const response = await fetch(
+      `${url}/v1/data/find`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${secrets.token}`,
+        },
+        body: JSON.stringify({
+          find: {},
+          projection: {},
+          skip: 0,
+          limit: 100,
+          sort: {},
+          dataset: payload.dataset,
+        }),
+      }
+    )
+
+    // Get results from response json
+    const data = await response.json();
+
+    return {
+      options: data.results,
+      value: data.results[0]
+    }
+  }
+};
